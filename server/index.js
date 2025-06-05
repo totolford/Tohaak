89c3a96d4240935960e6d6708df6011ab6599254
totol ford <totolford@gmail.com>
2025-06-05 22:08:00 +0200

    dev2

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
-
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const cors = require("cors");
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";  // <-- ici, on remplace require par import
 
 const app = express();
 app.use(cors());
+
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: { origin: "*" }
 });
