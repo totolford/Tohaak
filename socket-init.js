dcff424d57ce158993e825c7e3afd147a4d5299a
totolford <totolford@gmail.com>
2025-06-05 16:45:12 +0200

    bugfix test

--- socket-init.js
+++ socket-init.js
@@ -1,20 +1,19 @@
 // socket-init.js
+const socket = io("https://tohaak.onrender.com");
 
-// Connexion au serveur Socket.io
-const socket = io("https://tohaak.onrender.com/");
-
-// Rejoindre un quiz (à appeler dynamiquement selon le mode choisi)
-function joinQuiz(quizId, playerName) {
+// 🔓 Déclarer la fonction dans le scope global
+window.joinQuiz = function(quizId, playerName) {
     socket.emit("join_quiz", {
         quiz_id: quizId,
         player_name: playerName
     });
-}
+};
 
-// Gestion des événements de base
+// Réception de messages
 socket.on("player_joined", (data) => {
     console.log(`${data.player} a rejoint la partie !`);
 });
+
 
 socket.on("receive_question", (data) => {
     console.log("Question reçue :", data.question);
